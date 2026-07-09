a8c5457e786d63c0e1acda2c22bddd6af48a4cd1
vigorgotfa-design <vigorgotfa@gmail.com>
2026-07-09 14:44:09 +0200

    Update script.js

--- J's/script.js
+++ J's/script.js
@@ -1,1 +1,8 @@
-
+// Highlight active nav link
+const currentPage = window.location.pathname.split("/").pop();
+document.querySelectorAll('nav a').forEach(link => {
+  if(link.getAttribute('href') === currentPage) {
+    link.classList.add('active');
+  }
+});
+console.log("LFB Website Loaded - Lift Forward Better");
