caaef0c5216eb4ae03c916ada8515eca83e1fb8c
vigorgotfa-design <vigorgotfa@gmail.com>
2026-07-10 07:38:18 +0200

    Delete J's directory

--- J's/script.js
+++
@@ -1,8 +0,0 @@
-// Highlight active nav link
-const currentPage = window.location.pathname.split("/").pop();
-document.querySelectorAll('nav a').forEach(link => {
-  if(link.getAttribute('href') === currentPage) {
-    link.classList.add('active');
-  }
-});
-console.log("LFB Website Loaded - Lift Forward Better");
